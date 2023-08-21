ac64e416e2e814291ad703b374a3b1178beabcb7
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2023-08-21 12:32:09 -0500

    shell script draft 1

---
+++ ubuntu_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Update package lib. & upgrade.
+echo "Fetching updates and applying upgrades..."
+sudo apt update && sudo apt upgrade -y
+
+#Install fav packages
+sudo apt install neofetch pydf speedtest-cli python3-pip git curl wget whois -y
+
+#Set my Timezone 
+echo "Setting my Timezone"
+sudo timedatectl set-timezone America/Denver
+echo "Updated timezone:"
+date
+
+#Setup my MOTD
+sudo chmod -x /etc/update-motd.d/*
+sudo touch /etc/motd
+
+echo "Script completed."
