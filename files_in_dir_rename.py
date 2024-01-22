61ce6aa26bcc38d7583c7e4062f341d4b2403f59
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2024-01-21 22:28:11 -0600

    Update files_in_dir_rename.py

--- files_in_dir_rename.py
+++ files_in_dir_rename.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-#Modify to fit, removes a portion of a file name from files in a directory. I used this to fix JellyFin media meta-detection issues.
+#Removes a portion of a file name from files within a defined directory.
 import os
-
-directory = r"H:\BulkMediaStorage\TV Shows\$TVSHOW\$SEASON"
-# https://stackoverflow.com/questions/37400974/error-unicode-error-unicodeescape-codec-cant-decode-bytes-in-position-2-3
-
-print("Starting Rename Job...")
-
-# https://stackoverflow.com/questions/43728163/python-replace-all-spaces-with-underscores-and-convert-to-lowercase-for-all-fil
-[os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('$REMOVALHERE', '').lower()) for f in os.listdir(directory)]
-
-print("Completed Rename Job Successfully")
+def main():
+    #
+    directory = r"S:\BulkMediaStorage\TV Shows\$TVSHOW\$SEASON"
+#https://stackoverflow.com/questions/37400974/error-unicode-error-unicodeescape-codec-cant-decode-bytes-in-position-2-3
+    print("Starting Rename Job...")
+#https://stackoverflow.com/questions/43728163/python-replace-all-spaces-with-underscores-and-convert-to-lowercase-for-all-fil
+    [os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('$WHAT2REMOVE', '').lower()) for f in os.listdir(directory)]
+    print("Completed Rename Job Successfully")
+    #
+if __name__ == "__main__":
+    main()
