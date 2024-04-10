d2bcb8c962987321069a554cc677ca8021c4a4c3
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2024-04-10 08:37:26 -0500

    gitea stack
    Didn't work btw

--- files_in_dir_rename.py
+++ files_in_dir_rename.py
@@ -3,11 +3,11 @@
 import os
 def main():
     #
-    directory = r"S:\BulkMediaStorage\TV Shows\$TVSHOW\$SEASON"
+    directory = r"S:\BulkMediaStorage\TV Shows\$SHOW"
 #https://stackoverflow.com/questions/37400974/error-unicode-error-unicodeescape-codec-cant-decode-bytes-in-position-2-3
     print("Starting Rename Job...")
 #https://stackoverflow.com/questions/43728163/python-replace-all-spaces-with-underscores-and-convert-to-lowercase-for-all-fil
-    [os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('$WHAT2REMOVE', '').lower()) for f in os.listdir(directory)]
+    [os.rename(os.path.join(directory, f), os.path.join(directory, f).replace('doctor who ', '').lower()) for f in os.listdir(directory)]
     print("Completed Rename Job Successfully")
     #
 if __name__ == "__main__":
